3822d7c15a6e2e42698d17e8d6030ed630a2fc3b
Jessica Titensky <jtitensky@stuy.edu>
2016-02-08 12:38:06 -0500

    fixed

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -8,11 +8,14 @@ public class Recursion implements hw01{
 	if(n<0){
 	    throw new IllegalArgumentException();
 	}
-	return sqrtt(n,n/2);
+	if(n==0){
+	    return 0;
+	}
+	return sqrtt(n,1);
     }
 
     public double sqrtt(double n, double g){
-	if(-.0000000001<(g*g-n)/n && (g*g-n)/n<.0000000001){
+	if(-.00001<(g*g-n)/n && (g*g-n)/n<.00001){
 	    return g;
 	}
 	return sqrtt(n,(n/g+g)/2);
