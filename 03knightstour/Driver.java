a7bf705b3a5887446b4b80ff22a3b80e2c8cfce8
Jessica Titensky <jtitensky@stuy.edu>
2016-02-12 20:44:47 -0500

    done

---
+++ 03knightstour/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+    public static void main(String[]args){
+
+	KnightBoard k=new KnightBoard(8,10);
+	k.solve();
+	k.printSolution();
+
+
+    }
+}
--- 03knightstour/KnightBoard.java
+++ 03knightstour/KnightBoard.java
@@ -15,9 +15,67 @@ public class KnightBoard{
 	}
     }
 
-    public solvee(int i, int j, int step){
+    public KnightBoard(int cols, int rows){
+	board=new int[rows+4][cols+4];
+	for(int i=0;i<rows+4;i++){
+	    for(int j=0;j<cols+4;j++){
+		if(i==0 || i==1 || i==rows+2 || i==rows+3 || j==0 || j==1 || j==cols+2 || j==cols+3){
+		    board[i][j]=-1;
+		}else{
+		    board[i][j]=0;
+		}
+	    }
+	}
+    }
+
+    public boolean solvee(int i, int j, int step){
 	if(step==board.length*board.length){
 	    return true;
 	}
+	if(board[i][j]!=0){
+	    return false;
+	}
+	for(int m=0;m<board.length;m++){
+	    for(int n=0;n<board[0].length;n++){
+		if(board[m][n]>=step){
+		    board[m][n]=0;
+		}
+	    }
+	}
+	board[i][j]=step;
+	return 
+	    solvee(i+1,j+2,step+1)||
+	    solvee(i+2,j+1,step+1)||
+	    solvee(i-1,j-2,step+1)||
+	    solvee(i-2,j-1,step+1)||
+	    solvee(i+1,j-2,step+1)||
+	    solvee(i-1,j+2,step+1)||
+	    solvee(i+2,j-1,step+1)||
+	    solvee(i-2,j+1,step+1);
+    }
+
+    public boolean solve(){
+	return solvee(2,2,1);
+    }
+
+    public void printSolution(){
+	String s="";
+	for(int i=2;i<board.length-2;i++){
+	    for(int j=2;j<board[0].length-2;j++){
+		if(board[i][j]<10){
+		    s+="0";
+		}
+		s+=board[i][j]+" ";
+	    }
+	    s+="\n";
+	}
+	System.out.println(s);
+    }
+
+
+}
+
+
+
 
 
