7ec5a8bde5e15d931ad8de4343a6fa6e58ec45fe
Jessica Titensky <jtitensky@stuy.edu>
2016-04-22 12:28:00 -0400

    tostring and add

---
+++ 12bstree/Driver.java
@@ -0,0 +1,26 @@
+public class Driver{
+    public static void main(String[]args){
+
+	BSTree<Integer> t=new BSTree<Integer>();
+	System.out.println(t.toString());
+	t.add(3);
+	System.out.println(t.toString());
+	t.add(1);
+	System.out.println(t.toString());
+	t.add(4);
+	System.out.println(t.toString());
+	t.add(5);
+	System.out.println(t.toString());
+	t.add(2);
+	System.out.println(t.toString());
+
+
+
+
+
+
+
+    }
+}
+
+				       
